9af834298f79e49bf5614e0c40582af437aa11a0
Uyarn <uyarnchen@gmail.com>
2021-05-30 01:21:30 +0800

    complete gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,61 @@
-let gulp = require('gulp');
-let ts = require('gulp-typescript')
+var gulp = require('gulp');
+var ts = require('gulp-typescript')
 var tsProject = ts.createProject("tsconfig.json");
 
-let uglify = require('gulp-uglify');
-let del = require('del');
+var uglify = require('gulp-uglify');
+var del = require('del');
 
 const paths = {
-  statics: {
+  html:{
+    src: 'src/*.html',
+    dest: 'dist/'
+  },
+  fonts:{
+    src: 'src/fonts/**',
+    dest: 'dist/fonts'
+  },
+  styles: {
     src: 'src/*.css',
     dest: 'dist/'
+  },
+  sounds:{
+    src: 'src/sounds/**',
+    dest: 'dist/sounds/'
+  },
+  images:{
+    src: 'src/images/**',
+    dest: 'dist/images/'
   }
 };
 
 const clean = () => del([ 'dist' ]);
 
-function statics() {
-  return gulp.src(paths.statics.src)
-    .pipe(gulp.dest(paths.statics.dest));
+function html() {
+  return gulp.src(paths.html.src)
+    .pipe(gulp.dest(paths.html.dest));
+}
+
+
+
+function styles() {
+  return gulp.src(paths.styles.src)
+    .pipe(gulp.dest(paths.styles.dest));
+}
+
+function fonts() {
+  return gulp.src(paths.fonts.src)
+    .pipe(gulp.dest(paths.fonts.dest));
+}
+
+function sounds() {
+  return gulp.src(paths.sounds.src)
+    .pipe(gulp.dest(paths.sounds.dest));
+}
+
+
+function images() {
+  return gulp.src(paths.images.src)
+    .pipe(gulp.dest(paths.images.dest));
 }
 
 function scripts() {
@@ -24,7 +63,11 @@ function scripts() {
   .pipe(tsProject()).js.pipe(uglify()).pipe(gulp.dest("dist"))
 }
 
-const build = gulp.series(clean, gulp.parallel(statics,scripts));
+const build = gulp.series(clean,gulp.parallel(styles,html,fonts,sounds,images),scripts);
 
+
+exports.clean = clean;
+exports.scripts = scripts;
+exports.styles = styles;
 
 exports.default = build;
